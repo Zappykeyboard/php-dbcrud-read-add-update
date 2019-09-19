eef34c8af23aaabf82fa8be91ac143d29cf3ec24
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-09-19 17:06:07 +0200

    implementati getInventory e printInventory

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 function init() {
   console.log("Hello World");
+  getInventory();
 }
 
 $(document).ready(init);
+
+//ottengo l'inventario
+function getInventory(){
+
+  $.ajax({
+    url: 'inventario.php',
+    method:'GET',
+    success:function(data){
+      console.log(data);
+      printInventory(data);
+    } ,
+    error: function(err){
+      console.log(err);
+      
+    }
+  })
+}
+
+//mostro l'inventario a schermo
+function printInventory(data){
+
+  var source = $('#item-template').html();
+  var template = Handlebars.compile(source);
+  var html;
+
+  for (var i = 0; i<data.length; i++){
+    var el = data[i];
+ 
+    html = template(el);
+    $('.container').append(html);
+  }
+
+}
